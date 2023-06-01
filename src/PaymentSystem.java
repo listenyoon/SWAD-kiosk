b87e6fb0cf8b33596e38366fda6b49e779e4794d
objectio <objection@kakao.com>
2023-06-01 14:05:07 +0900

    [FEAT] Implement processPay

--- src/PaymentSystem.java
+++ src/PaymentSystem.java
@@ -1,21 +1,21 @@
 import java.util.ArrayList;
 
 public class PaymentSystem {
-    private static String paymentInfo;
-    private static String cardInfo;
+    private static String paymentMethod;
+    private static String cardName;
     private static StringBuilder receiptInfo;
-    private static int price;
+    private static int sum;
     private static ArrayList<Menu> cartItems;
 
     public static void paymentInfo(String method, ArrayList<Menu> cartItemss, int sums) {
-        paymentInfo = method;
-        price = sums;
+        paymentMethod = method;
+        sum = sums;
         cartItems = cartItemss;
         Front.waitingForCard();
     }
     
     public static String cardInfo(String cardInfos) {
-        cardInfo = cardInfos;
+        cardName = cardInfos;
         String receipt = processPay();
         return receipt;
     }
@@ -23,19 +23,37 @@ public class PaymentSystem {
     private static String processPay(){
         System.out.println("processing...");
         receiptInfo = new StringBuilder();
-        receiptInfo.append("[ 영수증 ]\n");
         
-        int sum = 0;
+        
+        receiptInfo
+            .append("\n                   [ 영수증 ]                   \n")
+            .append("----------------------------------------------\n")
+            .append(" 상품명            단가      수량      금액 \n")
+            .append("----------------------------------------------\n");
         for (Menu item : cartItems) {
-            int price = (Controller.foodList.get(item.name)).price * item.count;;
-            sum += price;
-            receiptInfo.append(item.name + "   ₩ " + price + "\n");
+            int price = (Controller.foodList.get(item.name)).price;
+
+            receiptInfo.append(" " + item.name);
+            for (int i = 0; i < 13 - item.name.length(); i++) {
+                receiptInfo.append(" ");
+            }
+            receiptInfo.append(price);
+            for (int i = 0; i < 10 - String.valueOf(price).length(); i++) {
+                receiptInfo.append(" ");
+            }
+            receiptInfo.append(item.count);
+            for (int i = 0; i < 10 - String.valueOf(item.count).length(); i++) {
+                receiptInfo.append(" ");
+            }
+            receiptInfo.append((price * item.count) + " \n");
+
+            receiptInfo.append("   ㄴ 사이즈: " + item.size + " \n");
         }
 
-        receiptInfo.append("합계 : " + sum);
-
-        receiptInfo.append(cardInfo + "\n")
-            .append(cardInfo);
+        receiptInfo.append("==============================================\n");
+        receiptInfo.append("합계 : " + sum + "\n");
+        receiptInfo.append("결제수단 : " + paymentMethod + "(" + cardName + ")" + "\n");
+        
         return receiptInfo.toString();
     }
 }
