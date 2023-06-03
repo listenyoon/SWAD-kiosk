56842f3ba50625a0eed9de3f7cf661950e41126b
objectio <objection@kakao.com>
2023-06-04 01:09:17 +0900

    [FEAT] 🎉 Implement all features 🎉

--- src/Controller.java
+++ src/Controller.java
@@ -41,10 +41,16 @@ public class Controller {
         orderNumber = Order.getOrderInfo(method, isTakeOut);
     }
 
-    public static void cardInfo(String cardName) {
+    public static String cardInfo(String cardName) {
         receiptInfo = PaymentSystem.cardInfo(cardName);
+        return receiptInfo;
     }
     
+    public static String barcodeInfo(String coupon) {
+        receiptInfo = PaymentSystem.barcodeInfo(coupon);
+        return receiptInfo;
+    }
+
     public static String requestReceiptInfo(){
         return receiptInfo;
     }
--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,13 +1,18 @@
 public class Kiosk {
     public static void main(String args[]){
         boolean status = true;
+        String method = "card";
         while (status) {
             System.out.println("키오스크 주문을 시작합니다.");
             Front.touchScreen();
             Front.selectMenu();
             Front.accept();
-            Front.selectOrderInfo();
-            Front.insertCard();
+            method = Front.selectOrderInfo();
+            System.out.println(method);
+            if (method.equals("card"))
+                Front.insertCard();
+            else if (method.equals("barcode"))
+                Front.scanBarcode();
             Front.printReceipt();
             System.out.println("주문 및 결제 완료\n");
         }
